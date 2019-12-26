5d44debe9831521c464e4018b638b2d2028699b9
Wu Xiantao <1214633619@qq.com>
2019-12-26 15:10:30 +0800

    Initialization project

---
+++ dream_web/src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.jpg">
+    <HelloWorld msg="这是home组件传给公共组件的值"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'home',
+  components: {
+    HelloWorld
+  }
+}
+</script>
