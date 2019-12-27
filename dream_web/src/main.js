01d378d3917a899993b9fa8e51c2dedc33f92e36
Wu Xiantao <1214633619@qq.com>
2019-12-27 10:33:16 +0800

    project-el

--- dream_web/src/main.js
+++ dream_web/src/main.js
@@ -1,9 +1,15 @@
 import Vue from 'vue'
+// import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import { Button, Select } from 'element-ui'
 
 Vue.config.productionTip = false
+// Vue.use(ElementUI)
+Vue.component(Button.name, Button)
+Vue.component(Select.name, Select)
 
 new Vue({
   router,
--- dream_web/src/views/About.vue
+++ dream_web/src/views/About.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="about">
     <h1>详情页</h1>
+    <el-button>默认按钮</el-button>
   </div>
 </template>
